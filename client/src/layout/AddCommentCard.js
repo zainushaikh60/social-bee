4bb6b644811d2bef54f349a5b4e374b791be9008
Zain <zainushaikh60@gmail.com>
2020-10-09 15:40:06 +0500

    all

--- client/src/layout/AddCommentCard.js
+++ client/src/layout/AddCommentCard.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useRef } from 'react';
+import React, { Fragment } from 'react';
 
 const AddCommentCard = ({
   user,
@@ -13,56 +13,68 @@ const AddCommentCard = ({
   return (
     <Fragment>
       <div className='post-add-comment'>
-        <a href='#!'>
-          <img
-            src={
-              user.profilePicture === null ? user.avatar : user.profilePicture
-            }
-            className='user-img'
-          />
-        </a>
+        <div className='add-comm'>
+          <a href='#!'>
+            <img
+              src={
+                user.profilePicture === null ? user.avatar : user.profilePicture
+              }
+              className='user-img'
+            />
+          </a>
 
-        <div className='post-add-comment-text'>
-          <input
-            type='text'
-            placeholder='Write a comment'
-            onChange={(e) => setComment(e.target.value)}
-            ref={clearInput}
-          />
+          <div className='post-add-comment-text'>
+            <input
+              type='text'
+              placeholder='Write a comment'
+              onChange={(e) => setComment(e.target.value)}
+              ref={clearInput}
+            />
 
-          <input
-            type='file'
-            name='post-comment'
-            id='post-comment'
-            accept='.jpg, .jpeg, .png, .gif'
-            onChange={(e) =>
-              e.target.files && e.target.files[0].size > maxAllowedSize
-                ? (setAlert(
-                    `Image file size should be less than 5 mb`,
-                    'danger',
-                    'info-circle'
-                  ),
-                  (e.target.value = ''))
-                : setCommentImage(e.target.files[0])
-            }
-          />
+            <input
+              type='file'
+              name='post-comment'
+              id='post-comment'
+              accept='.jpg, .jpeg, .png, .gif'
+              onChange={(e) =>
+                e.target.files && e.target.files[0].size > maxAllowedSize
+                  ? (setAlert(
+                      `Image file size should be less than 5 mb`,
+                      'danger',
+                      'info-circle'
+                    ),
+                    (e.target.value = ''))
+                  : setCommentImage(e.target.files[0])
+              }
+            />
 
-          <label
-            for='post-comment'
-            className='btn btn-primary btn-comment btn-comment-image'
-          >
-            <i class='far fa-image'></i>
-          </label>
+            <label
+              for='post-comment'
+              className='btn btn-primary btn-comment btn-comment-image'
+            >
+              <i class='far fa-image'></i>
+            </label>
 
-          <button
-            className='btn btn-primary btn-comment'
-            onClick={() => {
-              onClick();
-              clear();
-            }}
-          >
-            Comment
-          </button>
+            <button
+              className='btn btn-primary btn-comment'
+              onClick={() => {
+                onClick();
+                clear();
+              }}
+            >
+              Comment
+            </button>
+          </div>
+        </div>
+
+        <div className='comment-image-preview'>
+          <p className='preview-heading'>Attached Image Preview:</p>
+          <div className='image-preview'>
+            <img src='/images/post.jpg'></img>
+            <a href='#!' className='comment-image-cancel'>
+              <i class='fas fa-times'></i>
+            </a>
+          </div>
         </div>
       </div>
     </Fragment>
