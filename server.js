f13f8c1540d5ea16ad4f1525ef3081a7b643dd70
Zain <zainushaikh60@gmail.com>
2020-10-27 20:18:19 +0500

    Production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 const Auth = require('./routes/auth');
 const Users = require('./routes/users');
 const Posts = require('./routes/posts');
@@ -19,13 +20,20 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the Social-Bee API' }));
-
 // Routes
 
 app.use('/api/auth', Auth);
 app.use('/api/users', Users);
 app.use('/api/posts', Posts);
+
+//
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
