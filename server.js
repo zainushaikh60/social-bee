0b90565bd0828078f069e1bb5e7ad05aadfcabb8
Zain <zainushaikh60@gmail.com>
2020-10-27 20:28:20 +0500

    Production

--- server.js
+++ server.js
@@ -26,9 +26,8 @@ app.use('/api/auth', Auth);
 app.use('/api/users', Users);
 app.use('/api/posts', Posts);
 
-//
-
 if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
